0b81534321456f15b697cc968f8aa7d207a543a3
Rusdianto <rusdianto2254@gmail.com>
2022-07-23 20:25:45 +0800

    Create papua_game_01.py
    Ayo coba sebutkan 3 Provinsi baru di Papua

---
+++ papua_game_01.py
@@ -0,0 +1,17 @@
+"""
+Project 2 hari ke 4 (Papua Game)
+"""
+#Rusdianto, S.Pd
+
+print("Hai apa kabar... Yepmum...")
+print("Ayo coba sebutkan 3 Provinsi baru di Papua")
+provinsi=input("Sebutkan nama Provinsi: ")
+if provinsi == "papua tengah":
+    print("iya betul, provinsi papua tenga ibu kotanya nabire")
+elif provinsi == "papua pegunungan":
+    print("iya betul, provinsi papua pegunungan ibu kotanya jayawijaya")
+elif provinsi == "papua selatan":
+    print("iya betu, papua selatan ibu kotanya merauke")
+else:
+    print("Provinsi yang anda sebutkan bukan Provinsi baru di Papua")
+    
